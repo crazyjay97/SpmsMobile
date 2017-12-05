3d7f062dd983309dab62c52e060216bffd7e53aa
BeNumberOne <13257033208@163.com>
2017-12-05 13:56:27 +0800

    增加了一个activity作为首页

---
+++ app/src/main/java/com/example/admin/spmsmobile/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.spmsmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZWJ on 2017/12/5 0005.
+ */
+public class HomeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
--- app/src/main/java/com/example/admin/spmsmobile/MainActivity.java
+++ app/src/main/java/com/example/admin/spmsmobile/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.admin.spmsmobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 //工具默认集成AppCompatActivity  与设置无标题栏有冲突 这里继承 Activity
 public class MainActivity extends Activity {
@@ -17,8 +18,12 @@ public class MainActivity extends Activity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
 		//将布局文件引入main_activity
 		setContentView(R.layout.activity_main);
-		TextView view = (TextView) findViewById(R.id.text);
-		view.setText("Hello World!");
-		view.setTextSize(30f);
+	}
+	//事件不加View参数 会找不到该方法引起报错
+	public void onClick(View view){
+		Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+		//清除activity 防止返回时推导此页面
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+		MainActivity.this.startActivity(intent);
 	}
 }
